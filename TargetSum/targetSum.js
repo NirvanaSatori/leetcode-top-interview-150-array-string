bdd7b7b29b119aa6ff36015126ea170593f69add
NirvanaSatori <morris24kariuki@gmail.com>
2024-02-09 23:59:52 +0300

    add code solution to target sum

--- TargetSum/targetSum.js
+++ TargetSum/targetSum.js
@@ -1,15 +1,12 @@
 const nums = [3,2,4]
 const sum = 6
-
 function targetSum(){
-    
-        for(let i = 0; i<nums.length; i++){
-            for(let j = i+1; j<nums.length; j++){
-                if(nums[i]+nums[j] === sum){
-                    return [i,j]
-                }
+    for(let i = 0; i<nums.length; i++){
+        for(let j = i+1; j<nums.length; j++){
+            if(nums[i]+nums[j] === sum){
+                return [i,j]
             }
         }
-    
+    }   
 }
-(targetSum())
+targetSum()
