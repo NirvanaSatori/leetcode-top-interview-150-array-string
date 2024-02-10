ad697ef2036e6f28463330ca1a8c73ff5078e3c4
NirvanaSatori <morris24kariuki@gmail.com>
2024-02-10 14:08:32 +0300

    created a better fn using hashmap

--- TargetSum/targetSum.js
+++ TargetSum/targetSum.js
@@ -1,12 +1,25 @@
 const nums = [3,2,4]
 const sum = 6
-function targetSum(){
-    for(let i = 0; i<nums.length; i++){
-        for(let j = i+1; j<nums.length; j++){
-            if(nums[i]+nums[j] === sum){
-                return [i,j]
-            }
+// function targetSum(){
+//     for(let i = 0; i<nums.length; i++){
+//         for(let j = i+1; j<nums.length; j++){
+//             if(nums[i]+nums[j] === sum){
+//                 return [i,j]
+//             }
+//         }
+//     }   
+// }
+// targetSum()
+
+function twoSum(nums, target) {
+    let hashmap = {}
+    for(let x = 0; x < nums.length; x++){
+        if(hashmap[target - nums[x]] !== undefined){
+            return [hashmap[target - nums[x]], x]
+        }else{
+            hashmap[nums[x]] = x
         }
-    }   
-}
-targetSum()
+    }
+};
+
+twoSum()
